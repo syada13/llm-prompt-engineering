c3ecd5a226684aceffc91180ea240dbb9c49518d
syadav <suresh23_himcs@rediffmail.com>
2025-02-23 16:52:26 +0530

    Add conversation and memory feature to bot

--- production-applications/ConversationalApplication/vanila_bot.py
+++ production-applications/ConversationalApplication/vanila_bot.py
@@ -5,6 +5,8 @@ from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
+from general_prompting_principles.recency_bias_order_matters import conversation
+
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 temperature = 0
@@ -17,3 +19,25 @@ messages = [
 
 output = chat(messages)
 print(output.content)
+
+from langchain.memory import ConversationBufferMemory
+memory = ConversationBufferMemory()
+conversation = ConversationChain(
+    llm =chat,
+    memory=memory,
+    Verbose=True
+)
+
+# Conversation between Chat bot and user with ability to keep conversation history in memory
+conversation.run("Hi there !")
+conversation.run("What is the most iconic place in India?")
+conversation.run("What kind of other events?")
+
+#Make conversation interactive
+while True:
+    query = input('you:')
+    if query == 'q':
+        break
+    output = conversation({'input':query})
+    print('User', query)
+    print('AI System', output['response'])
