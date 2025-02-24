b0f7522ffb3bb44b8c54b7c0c809628b91d65d48
syadav <suresh23_himcs@rediffmail.com>
2025-02-24 16:29:41 +0530

    Enable non-parametric knowledge capabilities to the bot

--- production-applications/ConversationalApplication/vanila_bot.py
+++ production-applications/ConversationalApplication/vanila_bot.py
@@ -41,3 +41,47 @@ while True:
     output = conversation({'input':query})
     print('User', query)
     print('AI System', output['response'])
+
+
+# Add a non-parametric knowledge
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+
+# Load Italy_travel.pdf
+raw_documents = PyPDFLoader("italy_travel.pdf").load()
+
+#Initialize TextSplitter to split loaded document
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1500,
+    chunk_overlap=200
+)
+
+# Split raw_documents using Text Splitter
+documents = text_splitter.split_documents(raw_documents)
+
+#Embed text chunks using the by default embedding model = text-embedding-ada-002 and save into FAISS vector store.
+db = FAISS.from_documents(documents,OpenAIEmbeddings())
+
+#Use ConversationBufferMemory to keep chat history in memory
+memory = ConversationBufferMemory(
+    memory_key ='chat_history',
+    return_messages=True
+)
+
+# Let's interact with Conversation now.
+# The ConversationalRetrievalChain uses a prompt template called CONDENSE_QUESTION_PROMPT,
+# which merges the last user’s query with the chat history, so that it results as just one query to the retriever.
+# If you want to pass a custom prompt, you can do so using the condense_question_prompt parameter in the ConversationalRetrievalChain.from_llm module.
+
+query_chain = ConversationalRetrievalChain.from_llm(
+    llm=chat,
+    retriever=db.as_retriever(),
+    memory=memory,
+    verbose=True
+)
+
+query_chain.run({'question':'Give me some review about the Tajmahal'})
